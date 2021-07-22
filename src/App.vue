6d74293c8c8e8e48413ea66374a7dca00d56eef8
Assylzhan <assylzhan.baimuratov@gmail.com>
2021-07-22 12:56:06 +0600

    FIX: sass-loader node sass $ added some .scss module

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <header class="v-header">
+      <div class="v-header__wrapper p-2"
+      >
+        <div class="v-header__data v-header__data--day">Tuesday, 30th</div>
+        <div class="v-header__data v-header__data--month">March</div>
+      </div>
+    </header>
+    <div class="v-header-title v-text--center m-2">
+      My TODO List
+    </div>
+    <div class="v-form-block">
+      <form action="" class="form">
+        <div class="v-form__control">
+          <div class="v-form__wrapper">
+            <i class="v-icon v-form__icon v-form__icon--search"></i>
+            <input type="text" class="v-form__input">
+            <button class="v-button v-button--search">Search</button>
+          </div>
+        </div>
+        <div class="v-form__control">
+          <div class="v-form__wrapper">
+            <i class="v-icon v-form__icon v-form__icon--plus"></i>
+            <input type="text" class="v-form__input">
+            <button class="v-button v-button--add">Add todo</button>
+          </div>
+        </div>
+      </form>
+      <div class="v-todo-list-wrapper">
+        <ul class="v-todo-list mt-30">
+          <li class="v-todo-list__item p-20">
+            <span class="v-icon v-todo-list__complete"></span>
+            <span class="v-todo-list__icon"></span>
+            <div class="v-todo-list__text">Feed the cat</div>
+            <span class="v-icon v-todo-list__edit"></span>
+            <span class="v-icon v-todo-list__remove"></span>
+          </li>
+        </ul>
+      </div>
+      <div class="v-toolbar">
+        <div class="v-toolbar__item">
+          <button class="v-toolbar__button">All</button>
+        </div>
+        <div class="v-toolbar__item">
+          <button class="v-toolbar__button">Done</button>
+        </div>
+        <div class="v-toolbar__item">
+          <button class="v-toolbar__button">Active</button>
+        </div>
+        <div class="v-toolbar__item v-toolbar__item--rm-all">
+          <button class="v-toolbar__button"><span></span></button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data () {
+      return {
+        imgUrl: 'assets/images/night.jpg'
+      }
+    },
+    computed: {},
+    components: {}
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'font-awesome/css/font-awesome.min.css'
+import './assets/scss/index.scss'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({ render: h => h(App) }).$mount('#app')
